3073d9f1c005f5aa4799ab632456539559b023a6
odivanmarcos2 <85162051+odivanmarco@users.noreply.github.com>
2022-12-20 20:35:32 -0300

    Create the WebApi and projects Domain, Repository,Application and Infra

---
+++ Infra.Repository/Repositories/AddCustomer/AddCustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Contracts.Repositories.AddCustomer;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repository.Repositories.AddCustomer
+{
+    public class AddCustomerRepository : IAddCustomerRepository
+    {
+        private readonly IList<Customer> _customer = new List<Customer>();
+        public void AddCustomer(Customer customer)
+        {
+            _customer.Add(customer);
+        }
+    }
+}
---
+++ WebApi/Controllers/AddCustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Contracts.UseCases.AddCustomer;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models.AddCustomer;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddCustomerController : ControllerBase
+    {
+        private readonly IAddCustomerUseCase _addCustomerUseCase;
+
+        public AddCustomerController(IAddCustomerUseCase addCustomerUseCase)
+        {
+            _addCustomerUseCase = addCustomerUseCase;
+        }
+
+        [HttpPost]
+        public IActionResult AddCostumer(AddCustomerInput input)
+        {
+            var customer = new Domain.Entities.Customer(input.Name, input.Email, input.Document);
+            _addCustomerUseCase.AddCustomer(customer);
+
+            return Created("", input);
+        }
+    }
+}
